33cab9eeb39b6faac4aafef14b8805b689886cbf
Mariam <mari.kifshidze@gmail.com>
2024-09-12 21:28:52 +0400

    Django templates

--- mariam_kipshidze_lecture/my_online_store/product/urls.py
+++ mariam_kipshidze_lecture/my_online_store/product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from product.views import example_view
+from product.views import personal_info, check_age
 
 urlpatterns = [
-    path('example_view/', example_view, name='example_view'),
+    path('personal_info/', personal_info, name='personal_info'),
+    path('check_age/', check_age, name='check_age'),
 ]
